5b77fecb1b6b9cd44096b3225df9008d9dfb9593
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-02 21:53:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,14 +2,14 @@
 # # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   
-  if string == string
+  if string == string("I LOVE YOU GRANDMA!")
+     "I LOVE YOU TOO PUMPKIN!"
     
     
   else if string.upcase == string
       "NO, NOT SINCE 1938!"
   
-  else  == ("I LOVE YOU GRANDMA!")
-     "I LOVE YOU TOO PUMPKIN!"
+  else  == 
       "HUH?! SPEAK UP, SONNY!"
 end
 
